d1376f75c13325cbc210a8a876ae94ec763904e6
Fural <fernandoalugo@gmail.com>
2014-12-22 11:46:10 -0430

    cambiando tamano del title

--- app/views/blank.blade.php
+++ app/views/blank.blade.php
@@ -6,7 +6,7 @@ Blank
 @section('section')
 <div class="row">
   <div class="col-lg-12">
-    <h1 class="page-header">{{{ $title or 'Default' }}}</h1>
+    <h2 class="page-header">{{{ $title or 'Default' }}}</h2>
       <h4><strong>Welcome</strong> to the Lualca Admin panel.</h4>
         <p>This is a basic template to edit.</p>
   </div>
